fa6fbf8ac81dd9cd4dfba262f7181d911a9d94b6
harshadakadam74 <kadamharshada@gmail.com>
2026-02-20 16:03:05 +0530

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+// while
+
+let index = 0;
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+// output
+// value od index is 0
+// value od index is 2
+// value od index is 4
+// value od index is 6
+// value od index is 8
+// value od index is 10
+
+let myarr = ['flash', 'batman', 'superman'];
+let array = 0;
+while (array < myarr.length) {
+    console.log(`value is ${myarr[array]}`);
+       array = array + 1
+}
+// output
+// value is flash
+// value is batman
+// value is superman 
+
+// do-while loop
+
+let score = 1;
+do {
+    console.log(` value is ${score}`);
+    score++
+} while (score <= 10);
+
+// output
